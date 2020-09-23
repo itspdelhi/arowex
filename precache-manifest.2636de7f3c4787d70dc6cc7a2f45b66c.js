f10b70861022d866f91d819ba892f18f980e24f4
itspdelhi <itspdelhi@gmail.com>
2020-09-24 01:53:43 +0530

    Updates

---
+++ precache-manifest.2636de7f3c4787d70dc6cc7a2f45b66c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d989087e66567b267285bd08d132cd",
+    "url": "/arowex/index.html"
+  },
+  {
+    "revision": "af399ebfebac65884ed1",
+    "url": "/arowex/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "11f35e40583eb8333004",
+    "url": "/arowex/static/css/main.01639be4.chunk.css"
+  },
+  {
+    "revision": "af399ebfebac65884ed1",
+    "url": "/arowex/static/js/2.6a5f0514.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/arowex/static/js/2.6a5f0514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f35e40583eb8333004",
+    "url": "/arowex/static/js/main.982996b2.chunk.js"
+  },
+  {
+    "revision": "9b41e066bb341be22412",
+    "url": "/arowex/static/js/runtime-main.8c8df4f8.js"
+  }
+]);
